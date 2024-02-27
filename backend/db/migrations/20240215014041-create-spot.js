2a761d90e582ec2c56af282e241dc86a6bbedb95
Oleh Paris <olehparis@gmail.com>
2024-02-26 20:11:33 -0500

    fixed spots migration

--- backend/db/migrations/20240215014041-create-spot.js
+++ backend/db/migrations/20240215014041-create-spot.js
@@ -1,4 +1,8 @@
 'use strict';
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -59,9 +63,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue:Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
+    options.tableName = "Spots"
     await queryInterface.dropTable('Spots');
   }
 };
